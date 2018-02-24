ff9e6f2908317267a7ff4ac3694b30fa8d12713f
davidcbk <dmedina@cubikosolutions.com>
2018-02-24 20:57:41 +0000

    Initial commit

---
+++ app/src/main/java/com/cubikosolutions/loginbd/loginconbd/activity/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.cubikosolutions.loginbd.loginconbd.activity.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://localhost:8082/proyecto/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://localhost:8082/proyecto/register.php";
+}
